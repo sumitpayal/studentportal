4dd4b2f114a27e382b7cc9590a6177b4dbe51ed9
sumitpayal <65112901+sumitpayal@users.noreply.github.com>
2020-07-14 10:03:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1766,7 +1766,8 @@ app.get("/addteacher",(req,res) =>{
       res.render("teacherDelete");
    });
 //Admin Place ends 
-app.listen(3000, function() {
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
   console.log("Server started on port 3000");
 });
 module.exports = app;
